dd85cdf7139c06ee3cdad390aa736fd945fed8e4
Thirumaleshb <63893075+Thirumaleshb@users.noreply.github.com>
2022-09-27 17:55:43 +0530

    27-09-2022

---
+++ Contro-Statements.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var task = new List<string>();
+            Console.WriteLine("Checking Survey Details");
+            double surveyed, unsurveyed;
+            surveyed = Q1Results.NumberSurveyed;
+            unsurveyed = surveyed - Q1Results.NumberResponded;
+            Console.WriteLine(unsurveyed);
+            Console.WriteLine(surveyed);
+
+
+            //Logical Operation
+
+            if(unsurveyed> surveyed)
+            {
+                Console.WriteLine("More Number Of People Not Responded");
+            }
+            else
+            {
+                Console.WriteLine("More Number Of People Responded");
+            }
+
+            //
+            if (Q1Results.FavoriteProduct == "Cappucino")
+            {
+                Console.WriteLine("Yes It is the Favourite Product");
+            }
+            else
+            {
+                Console.WriteLine("No it is not Favourite Product");
+            }
+
+            if (true)
+            {
+                task.Add("Task is added");
+                Console.WriteLine("It is Evaluated as True and task is added");
+            }
+
+            foreach (var item in task)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
