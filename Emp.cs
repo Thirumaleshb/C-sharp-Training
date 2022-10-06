a4072ad5bacfa398518f6dadcdd11593fe4ebb6c
Thirumaleshb <63893075+Thirumaleshb@users.noreply.github.com>
2022-10-06 17:35:58 +0530

    Generics  06-10-2022

---
+++ Emp.cs
@@ -0,0 +1,35 @@
+﻿using CoffeeShop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+     class Emp
+    {
+        public static void Main (String[] args)
+        {
+            employee emp = new employee();
+            emp.Id = 1;
+            emp.Name = "Thiru";
+            EmployeeRepository<employee> empr = new EmployeeRepository<employee>();
+            EmployeeRepository<organisation> empr1 = new EmployeeRepository<organisation>();
+            EmployyeeRepositorySub empr2 = new EmployyeeRepositorySub();
+            empr.add(emp);
+            empr.add( new employee{Id=2, Name="Bhavana"});
+            empr.save();
+
+            var vari=empr.GetById(2);
+            Console.WriteLine("The Employee Who Got Removed is " + vari.Name);
+
+
+            var org = new organisation();
+            empr1.add(new organisation {Id=11, FirstName="Thirumalsh" });
+            empr1.add(new organisation { Id =22, FirstName = "Bhavana Jain" });
+            empr1.save();
+            empr2.remove(emp);
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,82 @@
+﻿//// See https://aka.ms/new-console-template for more information
+////Console.WriteLine("Hello, World!");
+//using Microsoft.VisualBasic;
+//using System;
+//using System.Collections.Generic;
+////namespace stackclass;
+
+//class program
+//{
+//    static void Main(String[] args)
+//    {
+
+
+
+
+
+
+//        var stack1= new SimpleStack<string>();
+//        stack1.Push("Thiru");
+//        stack1.Push("Malesh");
+//        stack1.Push("B");
+
+//        string item1 = (string)stack1.Pop();
+//        Console.WriteLine(item1);
+//        Console.WriteLine($"The Final Value is {item1}");
+//        Console.WriteLine("The Value of Count is " + stack1.count);
+
+
+
+
+
+
+//        var stack = new SimpleStack<double>();
+//        stack.Push(1.2);
+//        stack.Push(2.4);
+//        stack.Push(3.6);
+
+//        double item = (double)stack.Pop();
+//        Console.WriteLine(item);
+//        Console.WriteLine($"The Final Value is {item}");
+//        Console.WriteLine("The Value of Count is " + stack.count);
+//    }
+
+//}
+
+namespace CoffeeShop;
+ class employee :BaseClass
+{
+    
+
+    public string ?Name {
+        get; set;
+    }
+
+    public override string ToString()
+    {
+        return ($"ID:{Id} Name:{Name}");
+    }
+}
+
+class organisation: BaseClass
+{
+    
+
+    public string? FirstName
+    {
+        get; set;
+    }
+
+    public override string ToString()
+    {
+        return ($"ID:{Id} FirstName:{FirstName}");
+    }
+}
+
+class BaseClass
+{
+    public int Id { get; set; } 
+}
+
+
+
