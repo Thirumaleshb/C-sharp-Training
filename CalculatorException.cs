8eaf2023decb98de59f047a74659be3ca180415a
Thirumaleshb <63893075+Thirumaleshb@users.noreply.github.com>
2022-10-12 18:09:44 +0530

    Exception Programs Practiced on 12-10-2022

---
+++ CalculatorException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    public class Calculator
+    {
+        public int Calculate(int number1, int number2, string operation)
+        {
+
+            string non = operation ?? throw new ArgumentNullException(nameof(operation), "This is a null operator and Can not be used to perform the exception");
+            if (operation == "/")
+            { 
+
+                try
+                {
+                    return Divide(number1, number2);
+                }
+                //return Divide(number1, number2);
+                catch (DivideByZeroException ex)
+                {
+                    //throw new ArithmeticException("Hey it is the Divisio Error",ex);
+                    throw ;
+
+                }
+            }
+            else
+            {
+                //throw new ArgumentOutOfRangeException(nameof(operation), "The Mathematical Operator is not found");
+                //Console.WriteLine("Unknown operation.");
+                //return 0;
+                throw new CalculationOperationNotSupportedException(operation);
+            }
+        }
+
+        private int Divide(int number, int divisor) => number / divisor;
+    }
+}
+
---
+++ Exception.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine());
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpperInvariant();
+
+
+            var calculator = new Calculator();
+            try
+            {
+                int result = calculator.Calculate(number1, number2, operation);
+                DisplayResult(result);
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine("Hey it is showing the arithmetic expression error"+ex);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("the Paramter you have entered is null " + ex.Message);
+            }
+            //catch (ArgumentOutOfRangeException ex)
+            //{
+            //    Console.WriteLine("Enter the Correct operator" + ex.Message);
+            //}
+            catch (CalculationOperationNotSupportedException ex)
+            {
+                Console.WriteLine("We are invoking Custom methods" + ex);
+                Console.WriteLine(ex.Message);
+            }
+            catch( Exception ex)
+            {
+                Console.WriteLine("Something is Wrong " + ex);
+            }
+
+
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(int result) => WriteLine($"Result is: {result}");
+    }
+}
+
+
+// FROM C# 9 "Top-level statements":
+
+//using ConsoleCalculator;
+//using static System.Console;
+
+
+//WriteLine("Enter first number");
+//int number1 = int.Parse(ReadLine());
+
+//WriteLine("Enter second number");
+//int number2 = int.Parse(ReadLine());
+
+//WriteLine("Enter operation");
+//string operation = ReadLine().ToUpperInvariant();
+
+
+//var calculator = new Calculator();
+//int result = calculator.Calculate(number1, number2, operation);
+//DisplayResult(result);
+
+
+//WriteLine("\nPress enter to exit.");
+//ReadLine();
+
+
+//void DisplayResult(int result)
+//{
+//    WriteLine($"Result is: {result}");
+//}
+
+
